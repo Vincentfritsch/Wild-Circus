8979ed4f85ab73aab462c3c8c95c65cf5501321c
Vincent <v.fritsch1981@gmail.com>
2020-07-23 01:08:32 +0200

    Registration and login done

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
